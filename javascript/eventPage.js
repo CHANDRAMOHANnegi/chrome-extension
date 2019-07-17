2be4ee91c3cc412862b8f249b583e48a82bc4f69
chandramohan negi <chandramohan@bitcs.in>
2019-07-17 17:17:21 +0530

    showing probability

--- javascript/eventPage.js
+++ javascript/eventPage.js
@@ -1,37 +1,38 @@
 // noinspection JSDeprecatedSymbols
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.todo == "showPageAction") {
+
         const {tabs} = chrome;
         tabs.query({active: true, currentWindow: true}, (tabs) => {
             chrome.pageAction.show(tabs[0].id)
         });
-
 
         chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
             let url = tabs[0].url;
             console.log(url)
         });
     }
+
 });
 
 
-
-var menuItem = {
-    "id": "Speak",
-    "title": "Speak",
-    "contexts": ["selection"]
-};
-
-chrome.contextMenus.create(menuItem);
-
-chrome.contextMenus.onClicked.addListener(function (clickData) {
-    if (clickData.menuItemId == "Speak" && clickData.selectionText) {
-        chrome.tts.speak(clickData.selectionText,
-            {
-                'rate': 0.7
-            });
-    }
-});
-
-
-
+//
+// const menuItem = {
+//     "id": "Speak",
+//     "title": "Speak",
+//     "contexts": ["selection"]
+// };
+//
+// chrome.contextMenus.create(menuItem);
+//
+// chrome.contextMenus.onClicked.addListener(function (clickData) {
+//     if (clickData.menuItemId == "Speak" && clickData.selectionText) {
+//         chrome.tts.speak(clickData.selectionText,
+//             {
+//                 'rate': 0.7
+//             });
+//     }
+// });
+//
+//
+//
--- javascript/popup.js
+++ javascript/popup.js
@@ -1,42 +1,93 @@
-window.onload = function (e) {
+window.onload = function () {
+
 
     let btnChange = document.getElementById("btnChange");
+    let inputBox = document.getElementById("apiUri");
+    inputBox.focus();
+
 
     btnChange.addEventListener("click", () => {
-        let uri = document.getElementById("apiUri").value;
 
+
+        let isValid = false;
+        let status = document.getElementById('status');
+
+        let re = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+
+        if (inputBox.value && !re.test(inputBox.value)) {
+            isValid = false;
+            status.innerText = "email is invalid"
+        } else {
+            isValid = inputBox.value;
+        }
+
+
+        if (!isValid) {
+            status.innerText = "Enter valid email"
+        }
+
+
+        let uri = inputBox.value;
         const {tabs} = chrome;
+
         tabs.query({active: true, currentWindow: true}, (tabs) => {
-            chrome.pageAction.show(tabs[0].id)
+            chrome.pageAction.show(tabs[0].id);
+
             let url = tabs[0].url;
             url = `https://www.google.com/search?q="${uri}"`;
             let URL = url;
+
             console.log(URL);
 
-            const string = ["did not match any documents", "No results found for"];
 
-            let datafound = true;
+            let dataFound = true;
 
-            axios({
-                method: 'get',
-                url: URL,
-            }).then((response) => {
-                console.log(response);
-                console.log(response.data);
+            //  call  to  backend to verify  email
 
-                const data = response.data;
+            isValid && axios({
+                url: "http://localhost:3000/email",
+                method: 'post',
+                data: {
+                    email: uri
+                },
+                header: {
+                    "Access-Control-Allow-Origin": "*",
+                },
+            }).then(backendResponse => {
+                const {probability} = backendResponse.data.value;
+                console.log(probability);
 
-                string.forEach((string) => {
-                    if (data.includes(string)) {
-                        datafound = false
-                    }
-                });
-                if (datafound) {
-                    alert("data found")
-                } else {
-                    alert("data not found")
+                if (probability === "100%") {
+                    console.log("email not found " + probability);
+                    status.innerText = "Email found"
+                } else if (probability === "50%") {
+                    //************** call  to  google *****************
+                    axios({
+                        method: 'get',
+                        url: URL,
+                    }).then(response => {
+                        const data = response.data;
+                        console.log(response);
+
+                        const string = ["did not match any documents", "No results found for"];
+                        string.forEach((string) => {
+                            if (data.includes(string)) {
+                                alert(data.includes(string));
+                                dataFound = false
+                            }
+                        });
+                        status.innerText = "loading...";
+                        if (dataFound) {
+                            status.innerText = 'Email found';
+                        } else {
+                            status.innerText = "Email not found"
+                        }
+                    });
                 }
-            });
+
+
+            }).catch(err => console.log(err));
+
         });
     })
 };
